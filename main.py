bd289a39cd59c8ce318a8491709a594543a02adb
dogskat <heffcan@gmail.com>
2024-12-16 15:42:04 -0700

    added roation and movement

--- main.py
+++ main.py
@@ -9,19 +9,33 @@ def main():
     print("Screen height:", SCREEN_HEIGHT)
 
     pygame.init()
-    game_clock = pygame.time.Clock()
-    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
+    dt = 0
+
+
     while True:
-        screen.fill("black")
-        player.draw(screen)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.display.flip()
+
+        for obj in updatable:
+            obj.update(dt)
+
+        screen.fill("black")
+        for obj in drawable:
+            obj.draw(screen)
+
         dt = game_clock.tick(60) / 1000
+
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
